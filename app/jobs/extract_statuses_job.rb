15b5284d602b28a33dc75e675cd27ad0c9b7b519
Mateusz Juraszek <meceo00@gmail.com>
2018-01-28 00:28:14 +0100

    fetch elevation

--- app/jobs/extract_statuses_job.rb
+++ app/jobs/extract_statuses_job.rb
@@ -6,12 +6,13 @@ class ExtractStatusesJob < ApplicationJob
     if response.code == 200
       hash = JSON.parse(response.body)
       hash["response"]["feedMessageResponse"]["messages"]["message"].each do |message|
+        new_record = false
         Status.find_or_create_by!(identifier: message["id"]) do |status|
           status.recorded_at = Time.at(message["unixTime"])
           status.data = message
-
-          FetchElevationJob.perform_later(status.id)
+          new_record = true
         end
+        FetchElevationJob.perform_later(status.id) if new_record
       end
     end
   end
